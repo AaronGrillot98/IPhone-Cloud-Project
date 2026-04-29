94d43ec456e9d4432eafa20b19c637694477ee8b
Aaron <aarongrill98@gmail.com>
2026-04-29 10:29:38 -0700

    remove copies.

--- cloud-devsecops-guardrail-pipeline/src/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Cloud DevSecOps Guardrail Pipeline package."""
--- cloud-devsecops-guardrail-pipeline/terraform/insecure/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "aws_region" {
-  description = "AWS region used for validation only. This training project should not be deployed."
-  type        = string
-  default     = "us-east-1"
-}
